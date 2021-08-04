4ff7c8acc3291476b86cf818e6f34ee9bad6b83d
KChithra <chithra.nethra05@gmail.com>
2021-08-04 13:51:04 +0530

    Test Selenium

---
+++ SDET18Selenium_Framework/src/test/java/com/sdet18Selenium/DemoSelenium.java
@@ -0,0 +1,12 @@
+package com.sdet18Selenium;
+
+import org.testng.annotations.Test;
+
+public class DemoSelenium {
+@Test
+public void DemoTestSel() {
+	
+	System.out.println("Hey, there im in Selenium framework");
+}
+	
+}
